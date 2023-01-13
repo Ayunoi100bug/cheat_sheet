766dad9013916c34ecb26b95e3ba6508a02c32ad
Julalak-eye <julalakza18@gmail.com>
2023-01-13 08:13:59 +0700

    commit titile: appBar
    what: ได้สร้างรูปแบ 4 แบบที่สามารถcopyเพื่อไปใช้ในแต่ละหน้าได้ และมีการstaticที่ต้องเรียกใช้บ่อยๆไว้
    why: ต้องcopyไปใช้เนื่องจากการมีปัญหาในส่วนของsidebar ที่ต้องสร้างตัวแปลและใช้ค่านั้นในการเรียกใช้sidebar
    how: มีสองไฟล์คือ custom_appbar ที่มีstaticที่สร้างไว้ กับรูปแบบทั้ง4ของappbar และทำในหน้าmain screen

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,32 +1,54 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cheat_sheet/res/colors.dart';
 import 'package:cheat_sheet/res/components/bottom_bar.dart';
+import 'package:cheat_sheet/res/components/custom_appbar.dart';
+import 'package:cheat_sheet/res/components/sidebar_menu.dart';
 import 'package:cheat_sheet/utils/routes/routes.gr.dart';
 import 'package:flutter/material.dart';
+
+final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AutoTabsScaffold(
-      appBarBuilder: (context, tabsRouter) => AppBar(),
-      routes: const [
-        HomeScreenRoute(),
-        ActivityScreenRoute(),
-        CreateSheetRoute(),
-        SheetListRoute(),
-        ProfileRoute(),
-      ],
-      bottomNavigationBuilder: (context, tabsRouter) => BottomNavigationBar(
-        items: BottomBar.listItemBottomBar,
-        currentIndex: tabsRouter.activeIndex,
-        onTap: tabsRouter.setActiveIndex,
-        elevation: BottomBar.elevation,
-        showSelectedLabels: BottomBar.showSelectedLabels,
-        showUnselectedLabels: BottomBar.showUnselectedLabels,
-        type: BottomBar.bottomNavigationBarType,
-        selectedItemColor: BottomBar.selectedItemColor,
-        unselectedItemColor: BottomBar.unSelectedItemColor,
+    return Scaffold(
+      key: _scaffoldKey,
+      endDrawer: SidebarMenu(),
+      body: AutoTabsScaffold(
+        appBarBuilder: (context, tabsRouter) => AppBar(
+          backgroundColor: CustomAppBar.appBarColor,
+          title: CustomAppBar.textLogo,
+          actions: [
+            CustomAppBar.coin,
+            CustomAppBar.notifications,
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                _scaffoldKey.currentState!.openEndDrawer();
+              },
+            ),
+          ],
+        ),
+        routes: const [
+          HomeScreenRoute(),
+          ActivityScreenRoute(),
+          CreateSheetRoute(),
+          SheetListRoute(),
+          ProfileRoute(),
+        ],
+        bottomNavigationBuilder: (context, tabsRouter) => BottomNavigationBar(
+          items: BottomBar.listItemBottomBar,
+          currentIndex: tabsRouter.activeIndex,
+          onTap: tabsRouter.setActiveIndex,
+          elevation: BottomBar.elevation,
+          showSelectedLabels: BottomBar.showSelectedLabels,
+          showUnselectedLabels: BottomBar.showUnselectedLabels,
+          type: BottomBar.bottomNavigationBarType,
+          selectedItemColor: BottomBar.selectedItemColor,
+          unselectedItemColor: BottomBar.unSelectedItemColor,
+        ),
       ),
     );
   }
