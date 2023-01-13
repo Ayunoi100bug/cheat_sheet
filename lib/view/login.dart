55a1958a1a1130115881a4a1f97eb20d443cf5bb
Pisanu Boonyaanan <peat2512@hotmail.com>
2023-01-13 21:29:05 +0700

    Commit title: Fix dependencies from lastest merge
    What: Fix that app has a problem from lastest merge
    
    Why: Why not fix a bug?
    
    How: Remove duplicate and unused dependencies

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,6 +1,5 @@
 import 'package:cheat_sheet/model/user.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cheat_sheet/res/button.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
