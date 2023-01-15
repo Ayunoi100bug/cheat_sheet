7ccde793aa9694c7412f55b8b2ec68651e40ff98
Bookkub <kittiboo68@gmail.com>
2023-01-15 14:00:26 +0700

    Commit title: Complete sidebar and fix responsive text
    What: make most responsive sidebar as figma design
    
    Why: to make comfortable sidebar usage
    
    How: Create drawer list component for create drawer and use responsive widget to make most responsive sidebar

--- lib/res/components/sidebar_menu.dart
+++ lib/res/components/sidebar_menu.dart
@@ -1,9 +1,15 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:cheat_sheet/res/colors.dart';
+import 'package:cheat_sheet/res/typo.dart';
 import 'package:cheat_sheet/utils/routes/routes.gr.dart';
 import 'package:cheat_sheet/utils/routes/routes_name.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../gap_dimension.dart';
+import 'drawer_list.dart';
 
 class SidebarMenu extends StatefulWidget {
   const SidebarMenu({super.key});
@@ -13,69 +19,83 @@ class SidebarMenu extends StatefulWidget {
 }
 
 class _SidebarMenuState extends State<SidebarMenu> {
-
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Drawer(
-          child: Column(
-            children: [
-              Image.network(
-                'https://i.pinimg.com/564x/66/54/99/665499c8cd2c5b902e504c7029a2079a.jpg',
-                width: 100,
-                height: 120,
-              ),
-              ListTile(
-                  leading: Icon(FluentSystemIcons.ic_fluent_settings_regular),
-                  title: Text('ตั้งค่าการแจ้งเตือน'),
-                  onTap: () {
-                    AutoRouter.of(context).push(LoginRoute());
-                  }),
-              ListTile(
-                  leading:
-                      Icon(FluentSystemIcons.ic_fluent_person_accounts_regular),
-                  title: Text('บัญชีของฉัน'),
-                  onTap: () {
-                    print("account");
-                  }),
-              ListTile(
-                  leading: Icon(FluentSystemIcons.ic_fluent_bank_regular),
-                  title: Text('ข้อมูลบัญชีธนาคาร'),
-                  onTap: () {
-                    print("bank");
-                  }),
-              ListTile(
-                  leading: Icon(FluentSystemIcons.ic_fluent_history_regular),
-                  title: Text('ประวัติการจ่ายเงิน'),
-                  onTap: () {
-                    print("history");
-                  }),
-              ListTile(
-                  leading: Icon(FluentSystemIcons.ic_fluent_payment_regular),
-                  title: Text('เติมเงิน'),
-                  onTap: () {
-                    print("payment");
-                  }),
-              ListTile(
-                leading: Icon(FluentSystemIcons.ic_fluent_style_guide_regular),
-                title: Text('แนะนำการใช้งาน'),
-                onTap: () {
-                  print("guide");
-                },
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ListTile(
-                      leading:
-                          Icon(FluentSystemIcons.ic_fluent_arrow_enter_regular),
-                      title: Text('เข้าสู่ระบบ'),
-                      onTap: () {
-                        print("Register");
-                      }),
-                ),
-              ),
-            ],
+      child: ListView(
+        children: [
+          Image.asset(
+            'assets/images/logo.png',
+            height: screenWidth < 480 ? screenHeight * 0.2 : screenHeight * 0.4,
           ),
-        );
-    }
+          CustomListTile(
+            icon: Icons.notifications_active_outlined,
+            title: 'ตั้งค่าการแจ้งเตือน',
+            onTap: () {
+              AutoRouter.of(context).push(LoginRoute());
+            },
+          ),
+          CustomListTile(
+            icon: FluentSystemIcons.ic_fluent_shield_regular,
+            title: 'บัญชีของฉัน',
+            onTap: () {
+              print("account");
+            },
+          ),
+          CustomListTile(
+            icon: FluentSystemIcons.ic_fluent_bank_regular,
+            title: 'ข้อมูลบัญชีธนาคาร',
+            onTap: () {
+              print("bank");
+            },
+          ),
+          CustomListTile(
+            icon: Icons.receipt_outlined,
+            title: 'ประวัติการจ่ายเงิน',
+            onTap: () {
+              print("history");
+            },
+          ),
+          CustomListTile(
+            icon: FontAwesomeIcons.coins,
+            title: 'เติมเงิน',
+            onTap: () {
+              print("payment");
+            },
+          ),
+          CustomListTile(
+            icon: Icons.cast_for_education_outlined,
+            title: 'แนะนำการใช้งาน',
+            onTap: () {
+              print("guide");
+            },
+          ),
+          SizedBox(
+            height: screenHeight * 0.36,
+          ),
+          ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Medium20px(
+                  text: 'เข้าสู่ระบบ',
+                  color: AppColors.tertiary600,
+                ),
+                Icon(
+                  Icons.login_outlined,
+                  color: AppColors.tertiary600,
+                ),
+              ],
+            ),
+            onTap: () {
+              print('Lopgin');
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
