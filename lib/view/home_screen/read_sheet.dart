1c54e60f19d5d181987ea905efe603b76935e97b
Chisanupong Wipachainun <artaoom21@gmail.com>
2023-01-21 11:03:56 +0700

    Commit title : เพิ่ม Route Ask Question
    What : เพิ่ม Route Ask Question และหน้า Ask Question
    
    Why : เพื่อให้ทำหน้าถามคำถามได้
    
    How : แก้ไฟล์ routes.dart และทำการสร้างไฟล์ ask_question.dart ขึ้นมา

--- lib/view/home_screen/read_sheet.dart
+++ lib/view/home_screen/read_sheet.dart
@@ -1,3 +1,5 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:cheat_sheet/utils/routes/routes.gr.dart';
 import 'package:flutter/material.dart';
 
 class ReadSheet extends StatefulWidget {
@@ -10,6 +12,18 @@ class ReadSheet extends StatefulWidget {
 class _ReadSheetState extends State<ReadSheet> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Read Sheet'),
+          InkWell(
+            child: Text('Ask Question'),
+            onTap: () {
+              AutoRouter.of(context).push(AskQuestionRoute());
+            },
+          )
+        ],
+      ),
+    );
   }
 }
