0dcef3971be80e9761219cbfcee0ed727064f92a
Bookkub <kittiboo68@gmail.com>
2023-01-26 10:23:16 +0700

    Commit title: สร้างโครงหน้าอ่านชีท
    What: สร้างโครงหน้าอ่านชีท เป็นโครงจริงๆ
    
    Why: จะได้สวยๆ
    
    How: เพิ่มไฟล์สำหรับจัดการ  bottomsheet ที่ใช้ในหน้าการอ่านชีท

--- lib/view/home_screen/read_sheet.dart
+++ lib/view/home_screen/read_sheet.dart
@@ -1,9 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cheat_sheet/res/typo.dart';
-import 'package:cheat_sheet/utils/routes/routes.gr.dart';
 import 'package:flutter/material.dart';
 
 import '../../res/colors.dart';
+import '../../res/components/bottom_sheet.dart';
 
 class ReadSheet extends StatefulWidget {
   final int sheetId;
@@ -13,27 +13,166 @@ class ReadSheet extends StatefulWidget {
   State<ReadSheet> createState() => _ReadSheetState();
 }
 
+late bool isOpen = false;
+final _scaffoldKey = GlobalKey<ScaffoldState>();
+
 class _ReadSheetState extends State<ReadSheet> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
-
     return Scaffold(
-      body: Column(
+      key: _scaffoldKey,
+      floatingActionButton: Container(
+        padding: !isOpen
+            ? EdgeInsets.only(bottom: screenWidth * 0.04)
+            : EdgeInsets.only(bottom: screenWidth * 0.18),
+        child: FloatingActionButton(
+          splashColor: AppColors.warning100,
+          backgroundColor: AppColors.warning200.withOpacity(0.7),
+          elevation: 0,
+          onPressed: () {},
+          child: const Icon(
+            Icons.back_hand_outlined,
+            color: AppColors.warning600,
+            size: 30,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: screenWidth,
+                padding: EdgeInsets.only(top: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height:
+                          isPortrait ? screenWidth * 0.10 : screenWidth * 0.05,
+                      width: screenWidth,
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: screenWidth,
+                            alignment: Alignment.center,
+                            child: Bold24px(text: "สถิติพื้นฐาน"),
+                          ),
+                          Container(
+                            alignment: Alignment.centerRight,
+                            padding:
+                                EdgeInsets.only(right: screenWidth * 0.075),
+                            // child: Icon(
+                            //   Icons.book_outlined,
+                            //   size: 36,
+                            //   color: AppColors.black700,
+                            // ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Stack(
+                children: [
+                  SizedBox(
+                    width: screenWidth * 0.9,
+                    height: isOpen ? screenHeight * 0.47 : screenHeight * 0.67,
+                    child: Image.network(
+                      'https://northphoenixprep.greatheartsamerica.org/wp-content/uploads/sites/21/2016/11/default-placeholder.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Container(
+                    width: screenWidth * 0.9,
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(
+                        top: screenWidth * 0.02, right: screenWidth * 0.02),
+                    child: Icon(
+                      Icons.bookmark_outline,
+                      size: 36,
+                      color: AppColors.black700,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: isOpen
+                    ? screenHeight * 0.11 - 37
+                    : screenHeight * 0.13 - 37,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 28, vertical: 2),
+                  decoration: BoxDecoration(
+                    color: AppColors.black400,
+                    border: Border.all(color: AppColors.black400),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Regular16px(
+                    text: "1/5",
+                    color: AppColors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Text('This is Read Sheet Page of Sheet number ' +
-              widget.sheetId.toString()),
-          InkWell(
-            child: Text('Ask Question'),
-            onTap: () {
-              AutoRouter.of(context)
-                  .push(AskQuestionRoute(sheetId: widget.sheetId));
-            },
-          )
+          Container(
+            alignment: Alignment.center,
+            color: AppColors.black300,
+            height: screenHeight * 0.03,
+            width: screenWidth,
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                minimumSize: Size.fromHeight(40),
+                side: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              child: Icon(
+                isOpen
+                    ? Icons.keyboard_double_arrow_down
+                    : Icons.keyboard_double_arrow_up,
+                color: AppColors.black900,
+              ),
+              onPressed: () {
+                setState(() {
+                  isOpen = !isOpen;
+                });
+              },
+            ),
+          ),
+          isOpen ? BottomSheetWidget() : Container(),
         ],
       ),
     );
   }
 }
+
+// return Scaffold(
+//       body: Column(
+//         children: [
+//           Text('This is Read Sheet Page of Sheet number ' +
+//               widget.sheetId.toString()),
+//           InkWell(
+//             child: Text('Ask Question'),
+//             onTap: () {
+//               AutoRouter.of(context)
+//                   .push(AskQuestionRoute(sheetId: widget.sheetId));
+//             },
+//           )
+//         ],
+//       ),
+//     );
