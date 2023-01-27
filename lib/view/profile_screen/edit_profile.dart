81e02daa72ae318a1db3c74386583bbf4e65a6d7
Julalak-eye <julalakza18@gmail.com>
2023-01-27 11:52:00 +0700

    commit titile: ปรับแก้เรื่องการเปลี่ยน state การ login
    what: มีการปรับให้หน้า edit profile มีการเปลี่ยน state ของการ login ได้
    why: เนื่องจากพีทเคยบอกว่าถ้าหน้า edit มันต้อง login แล้วกดจากหน้า profile ก่อนอยู่แล้วแสดงว่าไม่มีการเปลี่ยน state เพราะงั้นไม่ต้องใส่ _authChanges ก็ได้ถูกไหม แต่ผมลองตรวจสอบแล้วว่าถ้ากด logout ตอนอยู่หน้า edit แล้วมันยังค้างหน้า edit อยู่เลยต้องทำให้มันเปลี่ยน state ตอนแรกคิดว่าจะทำให้มันเปลี่ยนหน้ากลับไปเลยแต่มัน error ผมเลยทำได้แค่เหมือนหน้า profile คือขึ้นข้อความปกติ
    how: เปลี่ยนแค่ให้เหมือนหน้า profile คือเปลี่ยน state ได้

--- lib/view/profile_screen/edit_profile.dart
+++ lib/view/profile_screen/edit_profile.dart
@@ -28,7 +28,8 @@ class _EditProfileState extends State<EditProfile> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final _auth = FirebaseAuth.instance;
   final _firestore = FirebaseFirestore.instance;
-  Users users = Users(username: '', password: '', email: '', uid: '', profileImage: '');
+  Users users =
+      Users(username: '', password: '', email: '', uid: '', profileImage: '');
   final firebase_storage.FirebaseStorage storage =
       firebase_storage.FirebaseStorage.instance;
   File? _pickedImage;
@@ -85,150 +86,157 @@ class _EditProfileState extends State<EditProfile> {
     var isLandScape =
         MediaQuery.of(context).orientation == Orientation.landscape;
 
-    return StreamBuilder(builder: (context, AsyncSnapshot<User?> snapshot) {
-      if (snapshot.hasData) {
-        return StreamBuilder<DocumentSnapshot>(
-            stream: _firestore
-                .collection("users")
-                .doc(_auth.currentUser?.uid)
-                .snapshots(),
-            builder: (BuildContext context,
-                AsyncSnapshot<DocumentSnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: Text("This is login, but data is not load"),
-                );
-              } else {
-                Map<String, dynamic> data =
-                    snapshot.data!.data() as Map<String, dynamic>;
-                return Scaffold(
-                  body: SafeArea(
-                    child: Container(
-                      padding: EdgeInsets.only(
-                          top: isPortrait
-                              ? screenHeight * 0.016
-                              : screenWidth * 0.016),
-                      width: double.infinity,
-                      height: isPortrait ? screenHeight * 0.5 : double.infinity,
-                      alignment: Alignment.center,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: isPortrait
-                                  ? screenHeight * 0.2
-                                  : screenWidth * 0.2,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: AppColors.black300,
-                              ),
-                              child: _pickedImage == null
-                                  ? CircleAvatar(
-                                      backgroundImage:
-                                          NetworkImage(data['profileImage']),
-                                      radius: 100.0,
-                                    )
-                                  : CircleAvatar(
-                                      backgroundImage: FileImage(_pickedImage!),
-                                      radius: 100.0,
-                                    ),
-                            ),
-                            SizedBox(
-                              height: isPortrait
-                                  ? screenHeight * 0.012
-                                  : screenWidth * 0.012,
-                            ),
-                            SizedBox(
-                              height: isPortrait
-                                  ? screenHeight * 0.05
-                                  : screenWidth * 0.05,
-                              child: InkWell(
-                                child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Icon(FontAwesomeIcons.camera),
-                                      SizedBox(
-                                        width: screenWidth * 0.012,
-                                      ),
-                                      const Regular12px(text: 'เลือกรูปภาพ'),
-                                    ]),
-                                onTap: () {
-                                  _pickImage();
-                                },
-                              ),
-                            ),
-                            SizedBox(
-                              height: isPortrait
-                                  ? screenHeight * 0.012
-                                  : screenWidth * 0.012,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(
-                                  right: isPortrait
-                                      ? screenWidth * 0.45
-                                      : screenHeight * 0.45),
-                              child: const Align(
-                                alignment: Alignment.center,
-                                child: Medium16px(text: 'ชื่อ'),
-                              ),
-                            ),
-                            SizedBox(
-                              width: isPortrait
-                                  ? screenWidth * 0.5
-                                  : screenHeight * 0.5,
-                              child: Form(
-                                key: _formKey,
-                                child: MyTextFormField(
-                                    hintText: data['username'],
-                                    validator: RequiredValidator(
-                                        errorText:
-                                            'Please enter new username.'),
-                                    onSaved: (value) {
-                                      users.username = value!;
+    return StreamBuilder(
+        stream: _auth.authStateChanges(),
+        builder: (context, AsyncSnapshot<User?> snapshot) {
+          if (snapshot.hasData) {
+            return StreamBuilder<DocumentSnapshot>(
+                stream: _firestore
+                    .collection("users")
+                    .doc(_auth.currentUser?.uid)
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: Text("This is login, but data is not load"),
+                    );
+                  } else {
+                    Map<String, dynamic> data =
+                        snapshot.data!.data() as Map<String, dynamic>;
+                    return Scaffold(
+                      body: SafeArea(
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              top: isPortrait
+                                  ? screenHeight * 0.016
+                                  : screenWidth * 0.016),
+                          width: double.infinity,
+                          height:
+                              isPortrait ? screenHeight * 0.5 : double.infinity,
+                          alignment: Alignment.center,
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: isPortrait
+                                      ? screenHeight * 0.2
+                                      : screenWidth * 0.2,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: AppColors.black300,
+                                  ),
+                                  child: _pickedImage == null
+                                      ? CircleAvatar(
+                                          backgroundImage: NetworkImage(
+                                              data['profileImage']),
+                                          radius: 100.0,
+                                        )
+                                      : CircleAvatar(
+                                          backgroundImage:
+                                              FileImage(_pickedImage!),
+                                          radius: 100.0,
+                                        ),
+                                ),
+                                SizedBox(
+                                  height: isPortrait
+                                      ? screenHeight * 0.012
+                                      : screenWidth * 0.012,
+                                ),
+                                SizedBox(
+                                  height: isPortrait
+                                      ? screenHeight * 0.05
+                                      : screenWidth * 0.05,
+                                  child: InkWell(
+                                    child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          const Icon(FontAwesomeIcons.camera),
+                                          SizedBox(
+                                            width: screenWidth * 0.012,
+                                          ),
+                                          const Regular12px(
+                                              text: 'เลือกรูปภาพ'),
+                                        ]),
+                                    onTap: () {
+                                      _pickImage();
+                                    },
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: isPortrait
+                                      ? screenHeight * 0.012
+                                      : screenWidth * 0.012,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      right: isPortrait
+                                          ? screenWidth * 0.45
+                                          : screenHeight * 0.45),
+                                  child: const Align(
+                                    alignment: Alignment.center,
+                                    child: Medium16px(text: 'ชื่อ'),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: isPortrait
+                                      ? screenWidth * 0.5
+                                      : screenHeight * 0.5,
+                                  child: Form(
+                                    key: _formKey,
+                                    child: MyTextFormField(
+                                        hintText: data['username'],
+                                        validator: RequiredValidator(
+                                            errorText:
+                                                'Please enter new username.'),
+                                        onSaved: (value) {
+                                          users.username = value!;
+                                        }),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: isPortrait
+                                      ? screenHeight * 0.016
+                                      : screenWidth * 0.016,
+                                ),
+                                PrimaryButton(
+                                    text: 'เสร็จสิ้น',
+                                    onPressed: () {
+                                      uploadImage();
+                                      _formKey.currentState!.save();
+                                      _firestore
+                                          .collection('users')
+                                          .doc(data['uid'])
+                                          .update({
+                                            'username': users.username,
+                                          })
+                                          .then(
+                                              (value) => print("User Updated"))
+                                          .catchError((error) => print(
+                                              "Failed to update user: $error"));
+                                      _formKey.currentState!.reset();
+                                      AutoRouter.of(context).pop();
                                     }),
-                              ),
+                                SizedBox(
+                                  height: isPortrait
+                                      ? screenHeight * 0.016
+                                      : screenWidth * 0.016,
+                                ),
+                              ],
                             ),
-                            SizedBox(
-                              height: isPortrait
-                                  ? screenHeight * 0.016
-                                  : screenWidth * 0.016,
-                            ),
-                            PrimaryButton(
-                                text: 'เสร็จสิ้น',
-                                onPressed: () {
-                                  uploadImage();
-                                  _formKey.currentState!.save();
-                                  _firestore
-                                      .collection('users')
-                                      .doc(data['uid'])
-                                      .update({
-                                        'username': users.username,
-                                      })
-                                      .then((value) => print("User Updated"))
-                                      .catchError((error) => print(
-                                          "Failed to update user: $error"));
-                                  _formKey.currentState!.reset();
-                                  AutoRouter.of(context).pop();
-                                }),
-                            SizedBox(
-                              height: isPortrait
-                                  ? screenHeight * 0.016
-                                  : screenWidth * 0.016,
-                            ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                );
-              }
-            });
-      } else {
-        return const Center(
-          child: Text("This is page when not login"),
-        );
-      }
-    });
+                    );
+                  }
+                });
+          } else {
+            return const Center(
+              child: Text("This is page when not login"),
+            );
+          }
+        });
   }
 }
