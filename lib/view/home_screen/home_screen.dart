5264a15e89c667ae723b9f43eb459f16f054c022
Julalak-eye <julalakza18@gmail.com>
2023-01-29 22:12:14 +0700

    commit titile: แก้ไขหน้า home_screen
    what: ทำให้หน้าhome_screen เลื่อนได้
    why: เพราะไม่สามารถเลื่อนได้ในกรณีชีทเยออะ
    how: ครอบด้วยSingleChildScrollView

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -38,100 +38,103 @@ class _HomeScreenState extends State<HomeScreen>
           return Scaffold(
             resizeToAvoidBottomInset: false,
             body: SafeArea(
-              child: Column(
-                children: [
-                  // InkWell(
-                  //   onTap: () {
-                  //     AutoRouter.of(context).push(const TestUIRoute());
-                  //   },
-                  //   child: const Text('กด ตรงนี้ นะ บุ๊คคค'),
-                  // ),
-                  Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: screenWidth * 0.02)),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 35,
-                    child: Card(
-                      shape: const RoundedRectangleBorder(
-                        side: BorderSide(
-                          color: AppColors.secondary800,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(12)),
-                      ),
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.search,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    // InkWell(
+                    //   onTap: () {
+                    //     AutoRouter.of(context).push(const TestUIRoute());
+                    //   },
+                    //   child: const Text('กด ตรงนี้ นะ บุ๊คคค'),
+                    // ),
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: screenWidth * 0.02)),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 35,
+                      child: Card(
+                        shape: const RoundedRectangleBorder(
+                          side: BorderSide(
                             color: AppColors.secondary800,
                           ),
-                          Light20px(
-                            text: 'Search',
-                            color: AppColors.primary600,
-                          )
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                        child: Row(
+                          children: const [
+                            Icon(
+                              Icons.search,
+                              color: AppColors.secondary800,
+                            ),
+                            Light20px(
+                              text: 'Search',
+                              color: AppColors.primary600,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: screenWidth * 0.02)),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 4,
+                        horizontal: 16,
+                      ).copyWith(right: 0),
+                      child: Row(
+                        children: const [
+                          Medium20px(text: 'ชีทแนะนำสำหรับคุณ'),
                         ],
                       ),
                     ),
-                  ),
-                  Padding(
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: screenWidth * 0.02)),
+                    Padding(
                       padding:
-                          EdgeInsets.symmetric(vertical: screenWidth * 0.02)),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 4,
-                      horizontal: 16,
-                    ).copyWith(right: 0),
-                    child: Row(
-                      children: const [
-                        Medium20px(text: 'ชีทแนะนำสำหรับคุณ'),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: screenWidth * 0.02)),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: screenWidth * 0.038),
-                    child: GridView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: isPortrait ? 3 : 5,
-                        crossAxisSpacing: 12,
-                        mainAxisSpacing: 16,
-                        mainAxisExtent: isPortrait ? 200 : 250,
+                          EdgeInsets.symmetric(horizontal: screenWidth * 0.038),
+                      child: GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: isPortrait ? 3 : 5,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 16,
+                          mainAxisExtent: isPortrait ? 200 : 250,
+                        ),
+                        itemCount: documentCount,
+                        itemBuilder: (context, index) {
+                          var sheet = snapshot.data?.docs[index];
+                          return StreamBuilder<DocumentSnapshot>(
+                            stream: _firestore
+                                .collection("users")
+                                .doc(sheet?["uid"])
+                                .snapshots(),
+                            builder: (context, userSnapshot) {
+                              if (!userSnapshot.hasData ||
+                                  userSnapshot.data!.data() == null) {
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              } else {
+                                return Sheet(
+                                  authorImage:
+                                      userSnapshot.data?["profileImage"],
+                                  title: sheet?["sheetName"],
+                                  priceSheet: sheet?["price"],
+                                  username: userSnapshot.data?["username"],
+                                  sheetId: sheet?["sid"],
+                                );
+                              }
+                            },
+                          );
+                        },
+                        padding: const EdgeInsets.only(bottom: 8),
                       ),
-                      itemCount: documentCount,
-                      itemBuilder: (context, index) {
-                        var sheet = snapshot.data?.docs[index];
-                        return StreamBuilder<DocumentSnapshot>(
-                          stream: _firestore
-                              .collection("users")
-                              .doc(sheet?["uid"])
-                              .snapshots(),
-                          builder: (context, userSnapshot) {
-                            if (!userSnapshot.hasData ||
-                                userSnapshot.data!.data() == null) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            } else {
-                              return Sheet(
-                                authorImage: userSnapshot.data?["profileImage"],
-                                title: sheet?["sheetName"],
-                                priceSheet: sheet?["price"],
-                                username: userSnapshot.data?["username"],
-                                sheetId: sheet?["sid"],
-                              );
-                            }
-                          },
-                        );
-                      },
-                      padding: const EdgeInsets.only(bottom: 8),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           );
