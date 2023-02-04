c0178769d9db9eb99e4b75f6a181d323f4039abd
Julalak-eye <julalakza18@gmail.com>
2023-02-04 23:02:40 +0700

    commit titile: create component popup login
    what: popup login และ ปรับขนาดของ popup dialog นิดหน่อย
    why: เพื่อให้เรียกใช้งานได้ง่าย และโค๊ดเป็นระเบียบ
    how: มีการสร้างpopup login ด้วย AlertDialog  มีการใส่ SizedBox เพื่อความง่ายในการอ่านใน popup dialog  และทำrespon แล้ว

---
+++ lib/res/components/popup_login.dart
@@ -0,0 +1,40 @@
+import 'package:cheat_sheet/res/colors.dart';
+import 'package:cheat_sheet/res/typo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../button.dart';
+
+Widget Popup_Login(BuildContext context) {
+  double screenHeight = MediaQuery.of(context).size.height;
+  return AlertDialog(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    title: Column(
+      children: [
+        Icon(
+          Icons.login_outlined,
+          color: AppColors.primary600,
+          size: 40,
+        ),
+        SizedBox(
+          height: screenHeight * 0.008,
+        ),
+        Medium20px(text: 'กรุณาเข้าสู่ระบบก่อนใช้งาน'),
+      ],
+    ),
+    actions: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          PrimaryButton(
+            text: 'เข้าสู่ระบบ',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    ],
+  );
+}
