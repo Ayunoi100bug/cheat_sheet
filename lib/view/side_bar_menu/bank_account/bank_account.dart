f93f75eb7bdbec35acc5201ffbead4f6356fd37e
Julalak-eye <julalakza18@gmail.com>
2023-02-08 13:30:07 +0700

    commit titile: ลบตามคำแนะนำโจโจ้
    what: ลบที่ไม่ได้ใช้
    why: เพื่อให้โค๊ดสะอาดมากขึ้น
    how: ลบออกไป 1 บรรทัด

--- lib/view/side_bar_menu/bank_account/bank_account.dart
+++ lib/view/side_bar_menu/bank_account/bank_account.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cheat_sheet/utils/routes/routes.gr.dart';
-import 'package:cheat_sheet/view/side_bar_menu/bank_account/add_payment_methods.dart';
 import 'package:flutter/material.dart';
 
 import '../../../res/colors.dart';
