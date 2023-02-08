7e2cc3a2aa138c4e2087990de314b5f9b0f93e35
Julalak-eye <julalakza18@gmail.com>
2023-02-08 11:38:27 +0700

    commit titile: หน้าที่เกี่ยวข้องกับบัญชีทั้งหมด
    what: หน้าที่เกี่ยวข้อกับบัญชี 3 หน้า และทำ componenet 1 หน้า
    why: เพื่อนำไปใช้หลังบ้านต่อในเรื่องของapi ผู้กบัญชี
    how: ทำหน้าข้อมูลบัญชีธนาคาร หน้าเพิ่มการชำระเงิน และหน้าเพิ่มบัตร โดยมีการทไresponsive ทุกหน้าแล้ว แต่มีเพียงtruemoney เท่านั้น

--- lib/view/side_bar_menu/bank_account/bank_account.dart
+++ lib/view/side_bar_menu/bank_account/bank_account.dart
@@ -3,6 +3,10 @@ import 'package:cheat_sheet/utils/routes/routes.gr.dart';
 import 'package:cheat_sheet/view/side_bar_menu/bank_account/add_payment_methods.dart';
 import 'package:flutter/material.dart';
 
+import '../../../res/colors.dart';
+import '../../../res/components/custom_appbar.dart';
+import '../../../res/typo.dart';
+
 class BankAccount extends StatefulWidget {
   const BankAccount({super.key});
 
@@ -13,22 +17,128 @@ class BankAccount extends StatefulWidget {
 class _BankAccountState extends State<BankAccount> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
     return Scaffold(
+      appBar: AppBar(
+        leading: AutoLeadingButton(),
+        title: Regular20px(
+          text: 'ข้อมูลบัญชีธนาคาร',
+          color: AppColors.white,
+        ),
+        backgroundColor: CustomAppBar.appBarColor,
+      ),
       body: SafeArea(
-        child: Center(
+        child: SingleChildScrollView(
           child: Column(
             children: [
-              Text('นี่คือหน้าข้อมูลบัญชีธนาคาร'),
-              ElevatedButton(
-                onPressed: () {
+              Column(
+                children: [_mastercard(context)],
+              ),
+              InkWell(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      bottom: screenWidth * 0.04,
+                      top: isPortrait
+                          ? screenWidth * 0.03
+                          : screenWidth * 0.024),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: AppColors.black300),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: screenWidth * 0.12,
+                          right: screenWidth * 0.12,
+                          top: screenWidth * 0.024,
+                          bottom: screenWidth * 0.024),
+                      child: Regular16px(
+                        text: '+ เพิ่มวิธีการชำระเงิน',
+                        color: AppColors.secondary500,
+                      ),
+                    ),
+                  ),
+                ),
+                onTap: () {
                   AutoRouter.of(context).push(AddPaymentMethodsRoute());
                 },
-                child: Text('ไปหน้าเพิ่มวิธีการชำระเงิน'),
-              )
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget _mastercard(context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+  return Padding(
+    padding: EdgeInsets.only(
+        top: screenWidth * 0.04,
+        left: isPortrait ? screenWidth * 0.016 : screenWidth * 0.16,
+        right: isPortrait ? screenWidth * 0.016 : screenWidth * 0.16,
+        bottom: isPortrait ? screenWidth * 0.04 : screenWidth * 0.008),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: AppColors.black300),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(
+            left: isPortrait ? screenWidth * 0.04 : screenWidth * 0.044,
+            right: isPortrait ? screenWidth * 0.12 : screenWidth * 0.044,
+            top: screenWidth * 0.04,
+            bottom: screenWidth * 0.04),
+        child: Row(
+          children: [
+            Image.asset(
+              'assets/images/mastercard.png',
+              width: isPortrait ? 92 : 100,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: isPortrait ? screenWidth * 0.2 : screenWidth * 0.24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Regular16px(text: 'Mastercard **** 1234'),
+                  Regular12px(
+                    text: 'หมดอายุ 01/23',
+                    color: AppColors.black400,
+                  ),
+                  SizedBox(
+                    height:
+                        isPortrait ? screenWidth * 0.04 : screenWidth * 0.044,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width:
+                            isPortrait ? screenWidth * 0.3 : screenWidth * 0.14,
+                      ),
+                      InkWell(
+                        child: Regular12px(
+                          text: 'แก้ไข',
+                          color: AppColors.tertiary500,
+                          underline: true,
+                        ),
+                        onTap: () {
+                          print('edit');
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
