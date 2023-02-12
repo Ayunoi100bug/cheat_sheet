c8a59ccd13d0a5804b17bc9fc2b3bfb2f8efb573
Bookkub <kittiboo68@gmail.com>
2023-02-12 09:36:05 +0700

    Commit title: แก้ TextForm ใหม่
    What: แก้ TextFormField ให้สามารถใช้ได้ครอบคลุมทุกกรณี
    
    Why: เพื่อความสวยงามของโค้ด จากเดิมที่มีหลายอัน แต่ตอนนี้สามารถใช้เพียงอันเดียวครอบคลุมได้ทุกอย่างตามที่ต้องการ
    
    How: เพิ่ม max line, min line, fontsize และทำให้โค้ดไม่ขัดกัน

--- lib/res/components/form_field.dart
+++ lib/res/components/form_field.dart
@@ -10,6 +10,9 @@ class MyTextFormField extends StatelessWidget {
   final FormFieldSetter<String> onSaved;
   final TextInputType? keyboardType;
   final bool obscureText;
+  final int maxLine;
+  final int minLine;
+  final double fontSize;
 
   MyTextFormField({
     super.key,
@@ -20,6 +23,9 @@ class MyTextFormField extends StatelessWidget {
     required this.onSaved,
     this.keyboardType = TextInputType.text,
     this.obscureText = false,
+    this.maxLine = 1,
+    this.minLine = 1,
+    this.fontSize = 14,
   });
 
   @override
@@ -54,93 +60,19 @@ class MyTextFormField extends StatelessWidget {
             Radius.circular(10),
           ),
         ),
-        contentPadding: EdgeInsets.fromLTRB(20.0, 12.0, 20.0, 12.0),
+        contentPadding: EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 12.0),
         hintText: hintText,
         helperText: helperText,
         isDense: true,
       ),
-      style: TextStyle(fontSize: 14),
+      style: TextStyle(fontSize: fontSize),
       validator: validator,
       autovalidateMode: autovalidateMode,
       keyboardType: keyboardType,
       obscureText: obscureText,
       onSaved: onSaved,
-    );
-  }
-}
-
-class MyTextFormFieldLarge extends StatelessWidget {
-  final String? hintText;
-  final String? helperText;
-  final AutovalidateMode autovalidateMode;
-  final FormFieldValidator<String> validator;
-  final FormFieldSetter<String> onSaved;
-  final TextInputType? keyboardType;
-  final bool obscureText;
-
-  MyTextFormFieldLarge({
-    super.key,
-    this.hintText,
-    this.helperText,
-    required this.autovalidateMode,
-    required this.validator,
-    required this.onSaved,
-    this.keyboardType = TextInputType.text,
-    this.obscureText = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
-    var isLandScape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
-
-    return TextFormField(
-      decoration: InputDecoration(
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-              width: 1, color: AppColors.black800, style: BorderStyle.solid),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-        ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-              width: 1, color: AppColors.black800, style: BorderStyle.solid),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-              width: 1, color: AppColors.error500, style: BorderStyle.solid),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-              width: 1, color: AppColors.error500, style: BorderStyle.solid),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-        ),
-        contentPadding: EdgeInsets.symmetric(
-            vertical: isPortrait ? screenWidth * 0.12 : screenWidth * 0.05,
-            horizontal: isPortrait ? screenWidth * 0.03 : screenWidth * 0.01),
-        hintText: hintText,
-        helperText: helperText,
-        isDense: true,
-      ),
-      style: TextStyle(fontSize: 14),
-      validator: validator,
-      autovalidateMode: autovalidateMode,
-      keyboardType: keyboardType,
-      obscureText: obscureText,
-      onSaved: onSaved,
-      maxLines: null,
+      maxLines: maxLine,
+      minLines: minLine,
     );
   }
 }
