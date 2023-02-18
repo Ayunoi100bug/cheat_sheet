79e6d34ca69a44d287d4462b540b6d51a49c566a
Khananon Kamjumroon <khananon001@gmail.com>
2023-02-18 19:58:12 +0700

    commit title : query review แบบที่มีการ fileter แล้ว
    what : แสดงรีวิวแบบดูทั้งหมด, 1 ดาว, 2 ดาวและอื่นๆ และมีการปรับแก้ให้มี popup เด้งแจ้งเตือนหากคุณไม่ทำการใส่ดาวตอนที่สร้างรีวิว แต่ในส่วนของความคิดเห็นจะใส่หรือไม่ใส่ก็ได้ และทำให้ดาวใส่แบบเต็มดวงไม่มี .5
    
    why : เพื่อให้มองเห็นรีวิวที่สร้างขึ้นมากันก่อน
    
    how : ใช้ streamBuilder ปกติในการแสดงค่า ใส่ FlushbarPopup ลงในการสร้างรีวิว

--- lib/view/home_screen/create_review.dart
+++ lib/view/home_screen/create_review.dart
@@ -49,7 +49,6 @@ class _CreateReviewState extends State<CreateReview> {
                 alignment: Alignment.center,
                 child: RatingBar.builder(
                   minRating: 0,
-                  allowHalfRating: true,
                   itemBuilder: (context, _) => const Icon(
                     Icons.star,
                     color: AppColors.warning400,
@@ -85,27 +84,31 @@ class _CreateReviewState extends State<CreateReview> {
                   onPressed: () async {
                     _formKey.currentState!.save();
                     try {
-                      myCollection
-                          .createReviewCollection(
-                        _review.text,
-                        _review.rid = uuid.v4(),
-                        _review.reviewerId = _auth.currentUser!.uid,
-                        _review.sheetId = widget.sheetId,
-                        _review.rating,
-                        _review.like!,
-                      )
-                          .then(
-                        (value) {
-                          _formKey.currentState!.reset();
-                          AutoRouter.of(context).popUntilRoot();
-                          const String message = 'รีวิวสำเร็จแล้ว!';
-                          FlushbarPopup.successFlushbar(context, const Icon(Icons.reviews), message);
-                        },
-                      );
-
-                      await _firestoreDb.collection('sheet').doc(widget.sheetId).update({
-                        'review': FieldValue.arrayUnion([_review.rid])
-                      });
+                      if (_review.rating == 0) {
+                        const String message = 'กรุณาระบุคะแนนที่ท่านต้องการให้ก่อน!';
+                        FlushbarPopup.errorFlushbar(context, FlushbarIcon.errorIcon, message);
+                      } else {
+                        myCollection
+                            .createReviewCollection(
+                          _review.text,
+                          _review.rid = uuid.v4(),
+                          _review.reviewerId = _auth.currentUser!.uid,
+                          _review.sheetId = widget.sheetId,
+                          _review.rating,
+                          _review.like!,
+                        )
+                            .then(
+                          (value) {
+                            _formKey.currentState!.reset();
+                            AutoRouter.of(context).popUntilRoot();
+                            const String message = 'รีวิวสำเร็จแล้ว!';
+                            FlushbarPopup.successFlushbar(context, const Icon(Icons.reviews), message);
+                          },
+                        );
+                        await _firestoreDb.collection('sheet').doc(widget.sheetId).update({
+                          'review': FieldValue.arrayUnion([_review.rid])
+                        });
+                      }
                     } on FirebaseAuthException catch (e) {
                       FlushbarPopup.errorFlushbar(context, FlushbarIcon.errorIcon, e.toString());
                     }
--- lib/view/review_sub_page/all.dart
+++ lib/view/review_sub_page/all.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../res/components/review.dart';
@@ -8,12 +10,55 @@ class All extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 2,
-      shrinkWrap: true,
-      itemBuilder: (context, index) {
-        return Container();
-      },
-    );
+    final _firestore = FirebaseFirestore.instance;
+
+    return StreamBuilder<DocumentSnapshot>(
+        stream: _firestore.collection("sheet").doc(sheetId).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            Map<String, dynamic> sheetData = snapshot.data!.data() as Map<String, dynamic>;
+            List? reviewInSheet = sheetData['review'];
+            reviewInSheet ??= [];
+            return ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: reviewInSheet.length,
+              itemBuilder: (BuildContext context, index) {
+                return StreamBuilder<DocumentSnapshot>(
+                    stream: _firestore.collection("review").doc(reviewInSheet![index]).snapshots(),
+                    builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> reviewSnapshot) {
+                      if (!reviewSnapshot.hasData) {
+                        return Container();
+                      } else if (reviewSnapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else {
+                        return StreamBuilder<DocumentSnapshot>(
+                          stream: _firestore.collection("users").doc(reviewSnapshot.data!['reviewerId']).snapshots(),
+                          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> userReviewSnapshot) {
+                            if (!userReviewSnapshot.hasData) {
+                              return Container();
+                            } else if (userReviewSnapshot.connectionState == ConnectionState.waiting) {
+                              return const Center(child: CircularProgressIndicator());
+                            } else {
+                              return Review(
+                                  userImage: userReviewSnapshot.data!['profileImage'],
+                                  userName: userReviewSnapshot.data!['username'],
+                                  userRating: reviewSnapshot.data!['rating'],
+                                  textReview: reviewSnapshot.data!['text'],
+                                  dateTime: reviewSnapshot.data!['timestamp'],
+                                  like: reviewSnapshot.data!['like']);
+                            }
+                          },
+                        );
+                      }
+                    });
+              },
+            );
+          }
+        });
   }
 }
