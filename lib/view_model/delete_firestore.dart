15b5ef0e6d2a1af1b6c66db2c612068b1ebd1b00
Chisanupong Wipachainun <artaoom21@gmail.com>
2023-02-20 10:58:55 +0700

    Commit title : แก้บัคลบชีท
    What :
    
    Why :
    
    How :

--- lib/view_model/delete_firestore.dart
+++ lib/view_model/delete_firestore.dart
@@ -2,6 +2,7 @@ import 'package:cheat_sheet/res/components/flushbar.dart';
 import 'package:cheat_sheet/res/components/flushbar_icon.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
 
 class DeleteCollection {
   final _firestore = FirebaseFirestore.instance;
@@ -10,9 +11,12 @@ class DeleteCollection {
     _firestore.collection("sheet").get().then((value) {
       value.docs.forEach((element) {
         FirebaseFirestore.instance.collection("sheet").doc(element.id).delete().then((value) {
-          Navigator.pop(context);
-          const String message = 'ลบชีททั้งหมดสำเร็จ';
-          FlushbarPopup.successFlushbar(context, FlushbarIcon.successIcon, message);
+          AutoRouter.of(context).pop();
+
+          Future.delayed(const Duration(milliseconds: 500), () {
+            const String message = 'ลบชีททั้งหมดสำเร็จ';
+            FlushbarPopup.successFlushbar(context, FlushbarIcon.successIcon, message);
+          });
         });
       });
     });
