4b240f2315b830d61d3eab0f8e01cee07f59db2a
Pisanu Boonyaanan <peat2512@hotmail.com>
2023-04-02 12:15:05 +0700

    Commit title: Change delete_firestore.dart too
    What: Change some get data to use data from read_firestore and change forEach to normal for loop
    
    Why: To make code prettier
    
    How: Change combo var&map into single var from read_firestore and change weird forEach into for loop

--- lib/view_model/delete_firestore.dart
+++ lib/view_model/delete_firestore.dart
@@ -3,6 +3,7 @@ import 'package:cheat_sheet/data/network/image_api.dart';
 import 'package:cheat_sheet/data/network/pdf_api.dart';
 import 'package:cheat_sheet/res/components/flushbar.dart';
 import 'package:cheat_sheet/res/components/flushbar_icon.dart';
+import 'package:cheat_sheet/view_model/read_firestore.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +11,9 @@ class DeleteCollection {
   final _firestore = FirebaseFirestore.instance;
 
   Future<void> deleteAllSheet(context) async {
-    _firestore.collection("sheet").get().then((value) {
-      value.docs.forEach((element) {
-        FirebaseFirestore.instance.collection("sheet").doc(element.id).delete().then((value) {
+    await _firestore.collection("sheet").get().then((value) {
+      for (var element in value.docs) {
+        _firestore.collection("sheet").doc(element.id).delete().then((value) {
           AutoRouter.of(context).pop();
 
           Future.delayed(const Duration(milliseconds: 500), () {
@@ -20,7 +21,7 @@ class DeleteCollection {
             FlushbarPopup.successFlushbar(context, FlushbarIcon.successIcon, message);
           });
         });
-      });
+      }
     });
   }
 }
@@ -30,10 +31,8 @@ class DeleteDocument {
 
   Future<void> deleteReview(BuildContext context, String reviewId, String sheetId) async {
     double result = 0;
-    var currentReviewSnapshot = await _firestore.collection("review").doc(reviewId).get();
-    Map<String, dynamic> currentReviewData = currentReviewSnapshot.data()!;
-    var currentSheetSnapshot = await _firestore.collection("sheet").doc(sheetId).get();
-    Map<String, dynamic> currentSheetData = currentSheetSnapshot.data()!;
+    var currentReviewData = await ReadReviewCollection().getParamsReviewData(reviewId);
+    var currentSheetData = await ReadCollection().getParamsSheetData(sheetId);
     List? reviewInSheet = currentSheetData['review'];
     reviewInSheet ??= [];
     result = ((currentSheetData['rating'] * reviewInSheet.length) - currentReviewData['rating']) / (reviewInSheet.length - 1);
@@ -41,7 +40,7 @@ class DeleteDocument {
       'review': FieldValue.arrayRemove([reviewId])
     });
     await _firestore.collection('sheet').doc(sheetId).update({'rating': result.isNaN ? 0.toDouble() : result});
-    _firestore.collection("review").doc(reviewId).delete().then((value) {
+    await _firestore.collection("review").doc(reviewId).delete().then((value) {
       AutoRouter.of(context).popUntilRoot();
       const String message = 'ลบความคิดเห็นสำเร็จ';
       FlushbarPopup.successFlushbar(context, FlushbarIcon.successIcon, message);
@@ -53,10 +52,7 @@ class DeleteDocument {
   }
 
   Future<void> deleteQuestion(BuildContext context, String questionId, String sheetId) async {
-    var currentReviewSnapshot = await _firestore.collection("question").doc(questionId).get();
-    Map<String, dynamic> currentReviewData = currentReviewSnapshot.data()!;
-    var currentSheetSnapshot = await _firestore.collection("sheet").doc(sheetId).get();
-    Map<String, dynamic> currentSheetData = currentSheetSnapshot.data()!;
+    var currentSheetData = await ReadCollection().getParamsSheetData(sheetId);
     List? questionInSheet = currentSheetData['question'];
     questionInSheet ??= [];
     await _firestore.collection('sheet').doc(sheetId).update({
@@ -84,32 +80,30 @@ class DeleteDocument {
     });
     await _firestore.collection('sheet').doc(sheetId).delete();
     await _firestore.collection('review').where('sheetId', isEqualTo: sheetId).get().then((value) {
-      value.docs.forEach((review) {
+      for (var review in value.docs) {
         _firestore.collection('review').doc(review.id).delete();
-      });
+      }
     });
     await PDFApi.deleteSheet(sheetId);
     await _firestore.collection('question').where('sheetId', isEqualTo: sheetId).get().then((value) {
-      value.docs.forEach((question) async {
+      for (var question in value.docs) {
         _firestore.collection('question').doc(question.id).delete();
-        await ImageApi.deleteQuestionImage(question.id);
-      });
+        ImageApi.deleteQuestionImage(question.id);
+      }
     });
-
     await _firestore.collection('sheetList').get().then((value) {
-      value.docs.forEach((list) async {
-        await _firestore.collection('sheetList').doc(list.id).update({
+      for (var list in value.docs) {
+        _firestore.collection('sheetList').doc(list.id).update({
           'sid': FieldValue.arrayRemove([sheetId])
         });
-      });
+      }
     });
-
     await _firestore.collection('users').get().then((value) {
-      value.docs.forEach((user) async {
-        await _firestore.collection('users').doc(user.id).update({
+      for (var user in value.docs) {
+        _firestore.collection('users').doc(user.id).update({
           'buyedSheet': FieldValue.arrayRemove([sheetId])
         });
-      });
+      }
     });
   }
 }
